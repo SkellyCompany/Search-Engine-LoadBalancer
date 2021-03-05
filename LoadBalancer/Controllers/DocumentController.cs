390444aa9955f8f82f5b0e7def8cb5549838a84e
schemabuoi <charyszczakg@gmail.com>
2021-03-05 11:12:53 +0100

    Add Document Controller (no implementation)

---
+++ LoadBalancer/Controllers/DocumentController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SearchEngine.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllDocumentsFromDocTable()
+        {
+            return Ok();
+        }
+    }
+}
