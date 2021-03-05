b809bed9c7adc200c39772585a3af6b27958502b
kidagine <davidkalatzis1999@hotmail.com>
2021-03-05 11:46:18 +0100

    Testing http client.

--- LoadBalancer/Controllers/DocumentController.cs
+++ LoadBalancer/Controllers/DocumentController.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,12 +6,21 @@ namespace SearchEngine.API.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+
+
     public class DocumentController : ControllerBase
     {
+        private readonly HttpClient client = new HttpClient();
+
 
         [HttpGet("{id:length(24)}")]
         public async Task<IActionResult> GetById(string id)
         {
+            HttpResponseMessage response = await client.GetAsync("document/id");
+            if (response.IsSuccessStatusCode)
+            {
+                //product = await response.Content.ReadAsAsync<Product>();
+            }
             return Ok();
         }
 
