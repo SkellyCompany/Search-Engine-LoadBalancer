926e01a5e5c4f8ee764361c3d475a55a0af3ec8d
schemabuoi <charyszczakg@gmail.com>
2021-03-05 12:13:09 +0100

    Update Document Controller

--- LoadBalancer/Controllers/DocumentController.cs
+++ LoadBalancer/Controllers/DocumentController.cs
@@ -1,8 +1,12 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using LoadBalancer.Entities;
 
-namespace SearchEngine.API.Controllers
+namespace LoadBalancer.Controllers
 {
     [Route("[controller]")]
     [ApiController]
@@ -14,20 +18,21 @@ namespace SearchEngine.API.Controllers
 
 
         [HttpGet("{id:length(24)}")]
-        public async Task<IActionResult> GetById(string id)
+        public async Task<IRestResponse> GetById(string id)
         {
-            HttpResponseMessage response = await client.GetAsync("document/id");
-            if (response.IsSuccessStatusCode)
-            {
-                //product = await response.Content.ReadAsAsync<Product>();
-            }
-            return Ok();
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("http://localhost:5002/document/");
+            var request = new RestRequest(id, Method.GET);
+            return client.Execute<Document>(request);
         }
 
         [HttpGet("all")]
-        public async Task<IActionResult> GetAllDocumentsFromDocTable()
+        public async Task<IRestResponse> GetAllDocumentsFromDocTable()
         {
-            return Ok();
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("http://localhost:5002/document/all");
+            var request = new RestRequest(Method.GET);
+            return client.Execute<Document>(request);
         }
     }
 }
