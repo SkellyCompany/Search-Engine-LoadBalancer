caecc6963beabc8d21a977bea841194278881cd3
Máté <34075771+leopoldus97@users.noreply.github.com>
2021-03-10 11:53:07 +0100

    Error forwarding

--- Controllers/DocumentController.cs
+++ Controllers/DocumentController.cs
@@ -9,32 +9,49 @@ namespace SearchEngine.LoadBalancer.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-
-
     public class DocumentController : ControllerBase
     {
+        private readonly string baseUrl = "http://localhost:5000/document/";
         private readonly HttpClient client = new HttpClient();
 
 
         [HttpGet("{id:length(24)}")]
         public async Task<IActionResult> GetById(string id)
         {
-            string html = "http://localhost:5000/document/" + id;
+            
+            
+            string html = baseUrl + id;
             HttpResponseMessage response = await client.GetAsync(html);
-            string result = await response.Content.ReadAsStringAsync();
-            Document document = JsonConvert.DeserializeObject<Document>(result);
-            return StatusCode((int)response.StatusCode, document); 
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Document document = JsonConvert.DeserializeObject<Document>(result);
+                return Ok(document);
+            } else
+            {
+                int statCode = (int)response.StatusCode;
+                string result = response.Content.ReadAsStringAsync().Result;
+                return StatusCode(statCode, result);
+            }
         }
 
 
         [HttpGet("all")]
         public async Task<IActionResult> GetAllDocumentsFromDocTable()
         {
-            string html = "http://localhost:5000/document/all";
+            string html = baseUrl + "all";
             HttpResponseMessage response = await client.GetAsync(html);
-            string result = await response.Content.ReadAsStringAsync();
-			List<Document> documents = JsonConvert.DeserializeObject<List<Document>>(result);
-            return StatusCode((int)response.StatusCode, documents);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                List<Document> documents = JsonConvert.DeserializeObject<List<Document>>(result);
+                return Ok(documents);
+            } else
+            {
+                int statCode = (int)response.StatusCode;
+                string result = response.Content.ReadAsStringAsync().Result;
+                return StatusCode(statCode, result);
+            }
         }
     }
 }
--- Controllers/TermController.cs
+++ Controllers/TermController.cs
@@ -13,15 +13,24 @@ namespace SearchEngine.LoadBalancer.Controllers
 	{
 		private readonly HttpClient client = new HttpClient();
 
+        private readonly string baseUrl = "http://localhost:5000/term/";
 
 		[HttpGet]
         public async Task<IActionResult> GetAllDocumentsForASpecificTerm([FromQuery] Request request)
         {
-			string html = $"http://localhost:5000/term/?PageNumber={request.PageNumber}&PageCount={request.PageNumber}&PageSize={request.PageNumber}&Keyword={request.PageNumber}";
+			string html = $"{baseUrl}?PageNumber={request.PageNumber}&PageCount={request.PageCount}&PageSize={request.PageSize}&Keyword={request.Keyword}";
 			HttpResponseMessage response = await client.GetAsync(html);
-			string result = await response.Content.ReadAsStringAsync();
-			List<DocumentInTerm> terms = JsonConvert.DeserializeObject<List<DocumentInTerm>>(result);
-			return Ok(terms);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                List<DocumentInTerm> terms = JsonConvert.DeserializeObject<List<DocumentInTerm>>(result);
+                return Ok(terms);
+            } else
+            {
+                int statCode = (int)response.StatusCode;
+                string result = response.Content.ReadAsStringAsync().Result;
+                return StatusCode(statCode, result);
+            }
 		}
     }
 }
