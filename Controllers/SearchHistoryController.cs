c6612688a4b724c05d131e8b1483fe4bf7ae9553
kidagine <davidkalatzis1999@hotmail.com>
2021-04-14 12:01:38 +0200

    Added search history controller.

---
+++ Controllers/SearchHistoryController.cs
@@ -0,0 +1,44 @@
+﻿using SearchEngine.LoadBalancer.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using LoadBalancer.LoadManager;
+using System.Text;
+using static SearchEngine.LoadBalancer.Entities.Log;
+
+namespace SearchEngine.LoadBalancer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SearchHistoryController : Controller
+    {
+        private readonly ILoadManager _loadManager;
+        private readonly HttpClient client = new HttpClient();
+
+        public SearchHistoryController(ILoadManager loadManager)
+        {
+            _loadManager = loadManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetHistory(string id)
+        {
+            string url = $"{_loadManager.GetNextHost()}/searchHistory/";
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                SearchHistory searchHistory = JsonConvert.DeserializeObject<SearchHistory>(result);
+                return Ok(searchHistory);
+            }
+            else
+            {
+                int statCode = (int)response.StatusCode;
+                string result = response.Content.ReadAsStringAsync().Result;
+                return StatusCode(statCode, result);
+            }
+        }
+    }
+}
---
+++ Entities/SearchHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SearchEngine.LoadBalancer.Entities
+{
+	public class SearchHistory
+	{
+		public string Keyword { get; set; }
+		public List<DateTime> Dates { get; set; }
+	}
+}
